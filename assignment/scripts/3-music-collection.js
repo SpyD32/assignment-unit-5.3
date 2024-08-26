b2966709da93b59e7afeeb7ce390dee76ac79f15
Michael Silas <michael.silas30@gmail.com>
2024-08-26 15:10:36 -0500

    showCollection function needs testing

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,25 +1,107 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
-let myCollection = []
-  
 
-  function addToCollection(title, artist, yearPublished) {
-    let newAlbum = {
-      artist:  'impendingDoom',
-    title: 'ripTearAndBurn',
-    yearPublished: '2013',
-    }
-    console.log(newAlbum);
+let myCollection = []
+ function addToCollection(array, object) {
+  array.push(object);
+  return object;
+ }
+
+
+
+
+ function showCollection(collection) {
+  console.log('should show my collection of albums');
+  for (let i = 0; i < collection; i++) {
+    console.log('title by artist, published in yearPublished');
+    
+    
+  }
+ }
+
+ showCollection('');
+
+
+ for (let i = 0; i <myCollection.length; i++) {
+ console.log('calling every item until the end', [i]);  
+ }
+
+
+let album1 = {
+  title: 'car',
+  artist: 'laxCity',
+  yearPublished: '2022',
+
+  
+   
+}
+
+let album2 = {
+  title: 'Hostage',
+  artist: 'ChelseaGrin',
+  yearPublished: '2018',
+
+  
+   
+}
+
+let album3 = {
+  title: 'death will reign',
+  artist: 'inpendingDoom',
+  yearPublished: '2002',
+
+  
+   
+}
+
+let album4 = {
+  title: 'aLittleGrace',
+  artist: 'yaahnHunter',
+  yearPublished: '2023',
+
+  
+   
+}
+
+let album5 = {
+  title: 'faultLines',
+  artist: 'latitudes',
+  yearPublished: '2022',
+
+  
+   
+}
+
+let album6 = {
+  title: 'brooklyn95',
+  artist: 'robotaki',
+  yearPublished: '2018',
+
+  
+   
+}
+
+let album7 = {
+  title: 'blackLikeYou',
+  artist: 'josephSolomon',
+  yearPublished: '2023',
+
+  
+   
+}
+
+addToCollection(myCollection, album1);
+addToCollection(myCollection, album2);
+addToCollection(myCollection, album3);
+addToCollection(myCollection, album4);
+addToCollection(myCollection, album5);
+addToCollection(myCollection, album6);
+addToCollection(myCollection, album7);
+
+console.log();
+
     
   
-
-  //addToCollection()
-myCollection.push(newAlbum)
-return newAlbum;
-  }
-
-addToCollection()
-
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
@@ -36,3 +118,36 @@ try {
 } catch (e) {
   // Do nothing
 }
+
+
+
+// function addToCollection(collection, title, artist, yearPublished) {
+//   const newAlbum = {
+//     title: title,
+//     artist: artist,
+//     yearPublished: yearPublished
+//   };
+//   collection.push(newAlbum);
+//   return newAlbum;
+// }
+
+// addToCollection(myCollection, 'car', 'laxCity', 2022);
+// addToCollection(myCollection, 'Hostage', 'ChelseaGrin', 2018);
+// addToCollection(myCollection, 'deathWillReign', 'impendingDoom', 2002 );
+// addToCollection(myCollection,'alittleGrace', 'yaahnHunter', 2023);
+// addToCollection(myCollection, 'brooklyn95', 'robotaki', 2018);
+// addToCollection(myCollection, 'blackLikeYou', 'josephSolomon', 2023);
+
+// function showCollection(collection) {
+//   console.log('albums in colection:' + collection.length);
+// for (let i = 0; i < collection.length; i++) {
+//  console.log();
+ 
+  
+// }
+  
+// }
+
+
+
+
